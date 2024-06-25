a18e9d4a443145455ed388b6fea19b39e344288a
Nirjla <shakyanirjala6@gmail.com>
2024-06-25 20:05:34 +0545

    error fixed

--- src/components/sections/banner-section.tsx
+++ src/components/sections/banner-section.tsx
@@ -7,16 +7,15 @@ import img1 from "@/assets/Young_woman_working_on_a_laptopYoung_woman_working_on
 
 export default function BannerSection() {
       return (<>
-            <div id="serviceSection">
+            <div id="servicesSection">
                   <SectionWrapper>
                         <MaxWidthWrapper>
                               <GridWrapper className="gap-y-0">
-                                    <HeroCard className="" title="Unlock Your Tech Future: Welcome to the IT Job Fair" highlight="Job Fair" desc="Embark on a Journey Towards Success as You Explore Diverse Tech Roles and Networking Opportunities" />
+                                    <HeroCard className="" title="Unlock Your Tech Future: Welcome to the IT Job Fair" highlight="Job Fair" desc="Embark on a Journey Towards Success as You Explore Diverse Tech Roles and Networking iOpportunities" />
                                     <HeroImg img={img1} className="order-2 h-[300px] md:h-[400px] lg:h-auto" />
                               </GridWrapper>
                         </MaxWidthWrapper>
                   </SectionWrapper>
             </div>
-
       </>)
 }
